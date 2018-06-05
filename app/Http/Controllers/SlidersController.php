b79901eb3452d7fbf8e089bf6e1c8cdf44143a6e
Fabricio Yassuda <fosmeo@gmail.com>
2018-06-05 07:17:13 -0300

    V.1.4

--- app/Http/Controllers/SlidersController.php
+++ app/Http/Controllers/SlidersController.php
@@ -91,7 +91,7 @@ class SlidersController extends Controller
       return redirect()->route('sliders.lista');
    }
 
-   public function gerenciadorSlidersAtualizarImagem(Request $request, $id_slider){
+   public function gerenciadorSliderAtualizarImagem(Request $request, $id_slider){
 
     $this -> validate($request,[
       'slider_imagem' => ['required']
@@ -112,39 +112,39 @@ class SlidersController extends Controller
     \Session::flash('flashmsg', 'IMAGEM DO SLIDER ATUALIZADA COM SUCESSO');
     return redirect()->route('sliders.lista');
    }
-   
+
    public function gerenciadorSlidersAtualizar(Request $request, $id_slider)
    {
 
     // RECEBE ARRAY INPUT DO REQUEST 0 ='pt' | 1 = 'en' | 2 = 'es' | 3 = 'it'
     Slider::where('tab_lang', 'LIKE', 'pt', 'AND', 'id_slider', '=', $id_slider) -> update(
       [
-      'slider_titulo' => $request -> slider_titulo[0],
-      'slider_texto' => $request -> slider_texto[0],
-      'slider_botao' => $request -> slider_botao[0],
-      'slider_link_botao' => $request -> slider_link_botao[0]
+        'slider_titulo' => $request -> slider_titulo[0],
+        'slider_texto' => $request -> slider_texto[0],
+        'slider_botao' => $request -> slider_botao[0],
+        'slider_link_botao' => $request -> slider_link_botao[0]
       ]);
 
     Slider::where('tab_lang', 'LIKE', 'en', 'AND', 'id_slider', '=', $id_slider) -> update(
       [
         'slider_titulo' => $request -> slider_titulo[1],
-      'slider_texto' => $request -> slider_texto[1],
-      'slider_botao' => $request -> slider_botao[1],
-      'slider_link_botao' => $request -> slider_link_botao[1]
+        'slider_texto' => $request -> slider_texto[1],
+        'slider_botao' => $request -> slider_botao[1],
+        'slider_link_botao' => $request -> slider_link_botao[1]
       ]);
     Slider::where('tab_lang', 'LIKE', 'es', 'AND', 'id_slider', '=', $id_slider) -> update(
       [
         'slider_titulo' => $request -> slider_titulo[2],
-      'slider_texto' => $request -> slider_texto[2],
-      'slider_botao' => $request -> slider_botao[2],
-      'slider_link_botao' => $request -> slider_link_botao[2]
+        'slider_texto' => $request -> slider_texto[2],
+        'slider_botao' => $request -> slider_botao[2],
+        'slider_link_botao' => $request -> slider_link_botao[2]
       ]);
     Slider::where('tab_lang', 'LIKE', 'it', 'AND', 'id_slider', '=', $id_slider) -> update(
       [
         'slider_titulo' => $request -> slider_titulo[3],
-      'slider_texto' => $request -> slider_texto[3],
-      'slider_botao' => $request -> slider_botao[3],
-      'slider_link_botao' => $request -> slider_link_botao[3]
+        'slider_texto' => $request -> slider_texto[3],
+        'slider_botao' => $request -> slider_botao[3],
+        'slider_link_botao' => $request -> slider_link_botao[3]
       ]);
 
     \Session::flash('flashmsg', 'SLIDER ATUALIZADO COM SUCESSO');
@@ -153,7 +153,7 @@ class SlidersController extends Controller
 
    }
 
-   public function g_botaoerenciadorSlidersExcluir($id_slider){
+   public function gerenciadorSlidersExcluir($id_slider){
 
       $slider_imagem = Slider::select('slider_imagem') -> where('id_slider', '=', $id_slider) -> first();
       $exclui_imagem = $slider_imagem -> slider_imagem;
