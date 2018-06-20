d4b15add565c4f07623fc5d57c35616be57c4e1c
Fabricio Yassuda <fosmeo@gmail.com>
2018-06-20 11:56:04 -0300

    V.1.4

--- resources/views/includes/inc-painel.blade.php
+++ resources/views/includes/inc-painel.blade.php
@@ -4,19 +4,19 @@
          <div class="cta-block">
             <div class="emmergency item">
                <i class="fa fa-phone"></i>
-               <p>Atendimento</p>
                <h2>{{ $traduz_global['painel_texto1'] }}</h2>
                	<a href="https://api.whatsapp.com/send?phone=5511999551260" target="_blank"><span>(11)</span> 9995-51260</a>
                   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </div>
             <div class="top-doctor item">
-               <i class="fa fa-stethoscope"></i>
+               <i class="fa fa-hourglass"></i>
                <h2>{{ $traduz_global['painel_texto2'] }}</h2>
                <a href="#" class="btn btn-main">{{ $traduz_global['botao1'] }}</a>
             </div>
             <div class="working-time item">
-               <i class="fa fa-hourglass-o"></i>
+               <i class="fa fa-flag-o"></i>
                <h2>{{ $traduz_global['painel_texto3'] }}</h2>
+               <a href="#" class="btn btn-main">{{ $traduz_global['botao1'] }}</a>
                {{-- <ul class="w-hours">
                   <li>Descontos Especiais em traduções para cidadania Italiana</li>
                   <li><a href="#" class="btn btn-main">Clique AQUI</a></li>
